36fc427abfb687e2cb22aa61ae196221035f5ad6
Andrea Barberio <insomniac@slackware.it>
2017-10-15 13:38:52 +0100

    Bumped to version 0.4.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ dublintraceroute = Extension(
 setup(
     name='dublintraceroute',
     packages=['dublintraceroute'],
-    version='0.4.0',
+    version='0.4.2',
     author='Andrea Barberio',
     author_email='insomniac@slackware.it',
     description='NAT-aware multipath traceroute',
